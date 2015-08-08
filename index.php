d66285d0aa363ae8c98986d0848e60ec4c6a6ad4
Rahul Kapoor <rahulkapoorbbps@outlook.com>
2015-08-08 23:34:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ echo json_encode($response);
 function getPriceFromFlipkart($url) {
  
 $curl = curl_init($url);
-curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 10.10; labnol;) ctrlq.org");
+curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 10.10;)");
 curl_setopt($curl, CURLOPT_FAILONERROR, true);
 curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
